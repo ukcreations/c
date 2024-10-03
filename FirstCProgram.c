e5e892ef1ac8e65b4fed265cc98bde6ac6a16347
ukcreations <122389134+ukcreations@users.noreply.github.com>
2024-10-03 16:03:11 -0700

    Update FirstCProgram.c

--- FirstCProgram.c
+++ FirstCProgram.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    printf("Hello World &&&&& Hellow PW");
+    printf("Hello World");
     printf("\n");
     printf("\nMaths is my favourite subject");
     return 0;
